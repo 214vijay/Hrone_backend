c7450eaa6a28c439708ddadc104db432d3443b48
214vijay <hi.gatecse@gmail.com>
2025-07-21 07:23:13 +0530

    Fix MongoDB URI usage and deploy setup

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
+import os
 from fastapi import FastAPI, HTTPException, status, Query
 from pydantic import BaseModel, Field
 from typing import List, Optional, Dict, Any
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from bson import ObjectId 
+from bson import ObjectId
+from dotenv import load_dotenv  
 
+load_dotenv()  
 
 app = FastAPI(
     title="Products and Orders API with MongoDB", 
-    description="A simple API task which manage products and orders, stored in MongoDB ." 
+    description="A simple API task which manages products and orders, stored in MongoDB."
 )
 
-# MongoDB connection 
-uri = "mongodb+srv://vijaykumarstage:KaxtyDoRIjtYMX4I@cluster0.3efaagg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+
+
+
 
 # Send a ping to confirm a successful connection
 try:
